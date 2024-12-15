ccb48bdf1f4336aea102e376606010a67fd394aa
Andre Pham <andrepham33@gmail.com>
2024-12-15 18:01:07 +1100

    cleanup

--- Sources/SwiftMath/SMPolyline.swift
+++ Sources/SwiftMath/SMPolyline.swift
@@ -108,128 +108,66 @@ open class SMPolyline: SMMutableGeometry, SMClonable {
         assert(tripletPointDistances.count == triplets.count)
         
         for (index, triplet) in triplets.enumerated() {
-            if index == 0 {
-                let originToCorner = SMLineSegment(origin: triplet.origin, end: triplet.corner)
-                let cornerToEnd = SMLineSegment(origin: triplet.corner, end: triplet.end)
-                let tripletIsStraight = originToCorner.origin.length(to: cornerToEnd.end).isEqual(to: originToCorner.length + cornerToEnd.length)
-                
-                // The applied point distance (max that can be done)
-                // E.g. if the point distance is 50, but the first line has a length of 10
-                let appliedPointDistance = tripletPointDistances[index]
-                
-                // The applied control point distance (max that can be done)
-                // E.g. if it's greater than the point distance
-                let maxControlPointDistance = appliedPointDistance
-                let appliedControlPointDistance = min(controlPointDistance, maxControlPointDistance)
-                
-                // Straight edge
-                let straightEdge = originToCorner.clone()
-                let straightEdgeDirection = straightEdge.angle!
-                straightEdge.adjustLength(by: -appliedPointDistance)
-                
-                // Next straight edge
-                let nextStraightEdge = cornerToEnd.clone()
-                let nextStraightEdgeDirection = nextStraightEdge.angle!
-                nextStraightEdge.adjustLength(by: -appliedPointDistance, anchorEnd: true)
-                
-                // Curved edge origin and origin control point
-                let origin = straightEdge.end.clone()
-                let originControlEdge = SMLineSegment(
-                    origin: straightEdge.origin,
-                    angle: straightEdgeDirection,
-                    length: straightEdge.length + appliedControlPointDistance
-                )
-                let originControlPoint = originControlEdge.end
-                
-                // Curved edge end and end control point
-                let end = nextStraightEdge.origin.clone()
-                let endControlEdge = SMLineSegment(
-                    origin: nextStraightEdge.end,
-                    angle: nextStraightEdgeDirection + SMAngle(radians: Double.pi),
-                    length: nextStraightEdge.length + appliedControlPointDistance
-                )
-                let endControlPoint = endControlEdge.end
-                
-                // Add to result
-                if straightEdge.length.isGreaterThanZero() {
-                    result.addLinearEdge(straightEdge)
-                }
-                if !tripletIsStraight {
-                    result.addBezierEdge(SMBezierCurve(
-                        origin: origin,
-                        originControlPoint: originControlPoint,
-                        end: end,
-                        endControlPoint: endControlPoint
-                    ))
-                }
-                
-                if index == triplets.count - 1 {
-                    if nextStraightEdge.length.isGreaterThanZero() {
-                        result.addLinearEdge(nextStraightEdge)
-                    }
-                }
-            } else {
-                let originToCorner = SMLineSegment(origin: triplet.origin, end: triplet.corner)
-                let cornerToEnd = SMLineSegment(origin: triplet.corner, end: triplet.end)
-                let tripletIsStraight = originToCorner.origin.length(to: cornerToEnd.end).isEqual(to: originToCorner.length + cornerToEnd.length)
-                
-                // The applied point distance (max that can be done)
-                // E.g. if the point distance is 50, but the first line has a length of 10
-                let appliedPointDistance = tripletPointDistances[index]
-                let previousAppliedPointDistance = tripletPointDistances[index - 1]
-                
-                // The applied control point distance (max that can be done)
-                // E.g. if it's greater than the point distance
-                let maxControlPointDistance = appliedPointDistance
-                let appliedControlPointDistance = min(controlPointDistance, maxControlPointDistance)
-                
-                // Straight edge
-                let straightEdge = originToCorner.clone()
-                let straightEdgeDirection = straightEdge.angle!
-                straightEdge.adjustLength(by: -appliedPointDistance)
-                
-                // Next straight edge
-                let nextStraightEdge = cornerToEnd.clone()
-                let nextStraightEdgeDirection = nextStraightEdge.angle!
-                nextStraightEdge.adjustLength(by: -appliedPointDistance, anchorEnd: true)
-                
-                // Curved edge origin and origin control point
-                let origin = straightEdge.end.clone()
-                let originControlEdge = SMLineSegment(
-                    origin: straightEdge.origin,
-                    angle: straightEdgeDirection,
-                    length: straightEdge.length + appliedControlPointDistance
-                )
-                let originControlPoint = originControlEdge.end
-                
-                // Curved edge end and end control point
-                let end = nextStraightEdge.origin.clone()
-                let endControlEdge = SMLineSegment(
-                    origin: nextStraightEdge.end,
-                    angle: nextStraightEdgeDirection + SMAngle(radians: Double.pi),
-                    length: nextStraightEdge.length + appliedControlPointDistance
-                )
-                let endControlPoint = endControlEdge.end
-                
-                // Add to result
-                // Adjust straight edge here, otherwise if its length is zero, its length can't be adjusted above (0 length has no direction)
-                straightEdge.adjustLength(by: -previousAppliedPointDistance, anchorEnd: true)
-                if straightEdge.length.isGreaterThanZero() {
-                    result.addLinearEdge(straightEdge)
-                }
-                if !tripletIsStraight {
-                    result.addBezierEdge(SMBezierCurve(
-                        origin: origin,
-                        originControlPoint: originControlPoint,
-                        end: end,
-                        endControlPoint: endControlPoint
-                    ))
-                }
-                
-                if index == triplets.count - 1 {
-                    if nextStraightEdge.length.isGreaterThanZero() {
-                        result.addLinearEdge(nextStraightEdge)
-                    }
+            let originToCorner = SMLineSegment(origin: triplet.origin, end: triplet.corner)
+            let cornerToEnd = SMLineSegment(origin: triplet.corner, end: triplet.end)
+            let tripletIsStraight = originToCorner.origin.length(to: cornerToEnd.end).isEqual(to: originToCorner.length + cornerToEnd.length)
+            
+            // The applied point distance (max that can be done)
+            // E.g. if the point distance is 50, but the first line has a length of 10
+            let appliedPointDistance = tripletPointDistances[index]
+            let previousAppliedPointDistance = index == 0 ? 0.0 : tripletPointDistances[index - 1]
+            
+            // The applied control point distance (max that can be done)
+            // E.g. if it's greater than the point distance
+            let maxControlPointDistance = appliedPointDistance
+            let appliedControlPointDistance = min(controlPointDistance, maxControlPointDistance)
+            
+            // Straight edge
+            let straightEdge = originToCorner.clone()
+            let straightEdgeDirection = straightEdge.angle!
+            straightEdge.adjustLength(by: -appliedPointDistance)
+            
+            // Next straight edge
+            let nextStraightEdge = cornerToEnd.clone()
+            let nextStraightEdgeDirection = nextStraightEdge.angle!
+            nextStraightEdge.adjustLength(by: -appliedPointDistance, anchorEnd: true)
+            
+            // Curved edge origin and origin control point
+            let origin = straightEdge.end.clone()
+            let originControlEdge = SMLineSegment(
+                origin: straightEdge.origin,
+                angle: straightEdgeDirection,
+                length: straightEdge.length + appliedControlPointDistance
+            )
+            let originControlPoint = originControlEdge.end
+            
+            // Curved edge end and end control point
+            let end = nextStraightEdge.origin.clone()
+            let endControlEdge = SMLineSegment(
+                origin: nextStraightEdge.end,
+                angle: nextStraightEdgeDirection + SMAngle(radians: Double.pi),
+                length: nextStraightEdge.length + appliedControlPointDistance
+            )
+            let endControlPoint = endControlEdge.end
+            
+            // Add to result
+            // Adjust straight edge here, otherwise if its length is zero, its length can't be adjusted above (0 length has no direction)
+            straightEdge.adjustLength(by: -previousAppliedPointDistance, anchorEnd: true)
+            if straightEdge.length.isGreaterThanZero() {
+                result.addLinearEdge(straightEdge)
+            }
+            if !tripletIsStraight {
+                result.addBezierEdge(SMBezierCurve(
+                    origin: origin,
+                    originControlPoint: originControlPoint,
+                    end: end,
+                    endControlPoint: endControlPoint
+                ))
+            }
+            
+            if index == triplets.count - 1 {
+                if nextStraightEdge.length.isGreaterThanZero() {
+                    result.addLinearEdge(nextStraightEdge)
                 }
             }
         }
